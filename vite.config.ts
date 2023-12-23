136ff3bb0dd7cbfb8aed182d30fb8416371f7419
victor-emanoel <victoremanoeldev@outlook.com>
2023-12-23 17:04:43 -0300

    chore: configure vite to build only "packages/index.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'packages/index.ts'),
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs', 'umd', 'iife'],
+      name: 'lib',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
