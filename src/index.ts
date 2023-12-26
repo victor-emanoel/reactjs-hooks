78bfd4b66c6deaf8945de75a25a6c6df00c23e51
victor-emanoel <victoremanoeldev@outlook.com>
2023-12-25 21:25:19 -0300

    feat: add useDebounce hook export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { useBoolean } from './useBoolean';
 export { useCounter } from './useCounter';
+export { useDebounce } from './useDebounce';
 export { useIsFirstRender } from './useIsFirstRender';
 export { useLocalStorage } from './useLocalStorage';
 export { useMousePosition } from './useMousePosition';
